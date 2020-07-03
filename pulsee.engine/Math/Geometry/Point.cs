e1b5e6b7b69165d0f9157506cb641a80892bfff8
Panda <pandav0x@hotmail.fr>
2020-07-03 18:29:04 +0200

    Remove unused using statement

--- pulsee.engine/Math/Geometry/Point.cs
+++ pulsee.engine/Math/Geometry/Point.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-
-namespace pulsee.engine.Math.Geometry
+﻿namespace pulsee.engine.Math.Geometry
 {
     class Point
     {
