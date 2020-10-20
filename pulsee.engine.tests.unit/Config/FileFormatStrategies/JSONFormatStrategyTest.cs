8cad759a37aec5abecb1800704cf7dc0126943f2
Panda <pandav0x@hotmail.fr>
2020-10-20 18:26:58 +0200

    Add unit tests solution

---
+++ pulsee.engine.tests.unit/Config/FileFormatStrategies/JSONFormatStrategyTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace pulsee.engine.tests.unit.Config.FileFormatStrategies
+{
+    public class JSONFormatStrategyTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void TestRead()
+        {
+            
+        }
+    }
+}
